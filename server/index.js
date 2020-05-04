52d0a7bda66bdce8deba19b2fb1f5f90a61ed402
Andrew Garvin <me@andrewgarv.in>
2020-05-04 13:34:40 -0600

    fix port number

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use("/", express.static(path.join(__dirname, "../public")));
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
